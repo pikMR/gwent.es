c692c1cef2f069547fb0d14f288622e9ea9b7575
unknown <pikamorfo@yahoo.com>
2016-09-23 16:07:18 +0200

    abstracción demo.php y arreglo de links

--- resources/views/includes/demo.blade.php
+++ resources/views/includes/demo.blade.php
@@ -1,47 +1,18 @@
 <div class="main">
 <div id="mi-slider" class="mi-slider">
-			<ul>
 
-				@for($i=0; $i < count($array_random[0]); $i++)
+				@for($j=0;$j<4;$j++)
+					<ul>
+					@for($i=0; $i < count($array_random[$j]); $i++)
 						<li>
-
-							<a href='{{ URL::asset('card/' .  $array_random[0][$i]->idCard ) }}'>
-									<img src='{{ URL::asset('assets/img/media/' . $array_random[0][$i]->filename) }}' alt="img0{{$i+1}}">
-										<h4>{{$array_random[0][$i]->name}}</h4>
+							<a href='{{ URL::asset('card/' .  $array_random[$j][$i]->idCard ) }}'>
+									<img src='{{ URL::asset('assets/img/media/' . $array_random[$j][$i]->filename) }}' alt="img0{{$i+1}}">
+										<h4>{{$array_random[$j][$i]->name}}</h4>
 							</a>
 						</li>
+					@endfor
+				</ul>
 				@endfor
-			</ul>
-			<ul>
-				@for($i=0; $i < count($array_random[1]); $i++)
-					<li>
-						<a href="#">
-								<img src='{{ URL::asset('assets/img/media/' . $array_random[1][$i]->filename) }}' alt="img0{{$i+1}}">
-									<h4>{{$array_random[1][$i]->name}}</h4>
-						</a>
-					</li>
-				@endfor
-			</ul>
-			<ul>
-				@for($i=0; $i < count($array_random[2]); $i++)
-					<li>
-						<a href="#">
-								<img src='{{ URL::asset('assets/img/media/' . $array_random[2][$i]->filename) }}' alt="img0{{$i+1}}">
-									<h4>{{$array_random[2][$i]->name}}</h4>
-						</a>
-					</li>
-				@endfor
-			</ul>
-			<ul>
-				@for($i=0; $i < count($array_random[3]); $i++)
-					<li>
-						<a href="#">
-								<img src='{{ URL::asset('assets/img/media/' . $array_random[3][$i]->filename) }}' alt="img0{{$i+1}}">
-									<h4>{{$array_random[3][$i]->name}}</h4>
-						</a>
-					</li>
-				@endfor
-			</ul>
 					<nav>
 						<a href="#">COMÚN</a>
 						<a href="#">ÉPICO</a>
